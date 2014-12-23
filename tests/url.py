0947cb67e6403e53fdad40abc96e3cfe2c480dcf
James Michael DuPont <JamesMikeDuPont@gmail.com>
2014-12-23 05:05:49 -0600

    update new languages

---
+++ tests/url.py
@@ -0,0 +1,2 @@
+url ='https://www.wikidata.org/w/api.php?maxlag=5&continue=&format=json&meta=siteinfo%7Cuserinfo&action=query&siprop=namespaces%7Cnamespacealiases%7Cgeneral&uiprop=blockinfo%7Chasmsg'
+
